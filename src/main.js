3813f82827e46da85909a4af9eaa6f5c5a5014bd
Johnathon Slightham <31053827+jslightham@users.noreply.github.com>
2021-02-17 20:00:17 -0500

    Vue Project Created

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+import Home from './components/Home.vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+const routes = [
+  {
+      name: 'home',
+      path: '/',
+      component: Home
+  },
+  {
+    name: 'hello',
+    path: '/hello',
+    component: HelloWorld
+}
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
