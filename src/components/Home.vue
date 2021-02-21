cb97f4bd5ab958c1f6673a4b99b3c83acbc8693f
Grant <grant@rbgn.ca>
2021-02-21 16:23:32 -0500

    Fixed home page sorta mostly

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,22 @@
 <template>
 <div>
   <section class="text-gray-400 bg-gray-900 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col text-center w-full mb-20">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">BrainCraft</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-pink-600">Description.</p>
-      <iframe src="https://discord.com/widget?id=804162605107249213&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-    </div>
-    <div class="flex flex-col text-center w-full mb-20">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">BrainNG</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-pink-600">Description.</p>
-      <iframe src="https://discord.com/widget?id=789657841895735337&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+  <div class="flex items-center">
+    <div class="container px-5 py-24 mx-auto space-y-6">
+      <div class="flex items-start space-x-5">
+        <div class="flex flex-col text-right w-full mb-20">
+          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">BrainCraft</h1>
+          <p class="leading-relaxed text-pink-600 text-right">Description.</p>
+        </div>
+        <iframe src="https://discord.com/widget?id=804162605107249213&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+      </div>
+      <div class="flex items-start space-x-5">
+        <div class="flex flex-col text-right w-full mb-20">
+          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">BrainNG</h1>
+          <p class="leading-relaxed text-pink-600 text-right">Description.</p>
+        </div>
+        <iframe src="https://discord.com/widget?id=789657841895735337&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+      </div>
     </div>
   </div>
 </section>
