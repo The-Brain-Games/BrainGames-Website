6d5beb013ad9a33f2afc5c443c567f6378087319
Grant <grant@rbgn.ca>
2021-03-10 19:49:12 -0500

    Added JohnS to About us

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -57,7 +57,7 @@
               </div>
               <div class="mb-2 text-gray-700">
                 <i class="fas fa-briefcase mr-2 text-lg text-gray-500"></i
-                >Founder of Brain Games & owner of BrainCraft
+                >Founder of Brain Games, Owner of BrainCraft & Developer for Brain Games
               </div>
             </div>
             <div class="mt-10 py-10 border-t border-gray-300 text-center">
@@ -138,10 +138,84 @@
               <div class="flex flex-wrap justify-center">
                 <div class="w-full lg:w-9/12 px-4">
                   <p class="mb-4 text-lg leading-relaxed text-gray-800">
-                    Hi, I'm PerfectSquare150 but you can call me square ;) I joined the Brain Games group back in September and brought with me the idea to host multiplayer BeamNG servers. Aside from video games, I love to tinker with electronics (especially 3D printers) and work as a mechanical lead on the Canadian FRC Team, <a href="https://www.instagram.com/frcteam7558/" style="text-decoration:underline; color:#575757" target="_blank" rel="noopener noreferrer">ALT F4</a>. In my spare time I love going for long drives and working on my car.
+                    Hi, I'm PerfectSquare150 but you can call me square. I joined the Brain Games group back in September and brought with me the idea to host multiplayer BeamNG servers. Aside from video games, I love to tinker with electronics (especially 3D printers) and work as a mechanical lead on the Canadian FRC Team, <a href="https://www.instagram.com/frcteam7558/" style="text-decoration:underline; color:#575757" target="_blank" rel="noopener noreferrer">ALT F4</a>. In my spare time I love going for long drives and working on my car.
                   </p>
                   <p class="mb-2 text-small leading-relaxed text-gray-600">
                     Note: If you ever refer to my car, call it a hair dryer.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+        <section class="relative block bg-gray-900" style="height: 300px;">
+      <div
+        class="absolute top-0 w-full h-full bg-center bg-cover"
+      >
+      </div>
+      <div
+        class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
+        style="height: 70px; transform: translateZ(0px);"
+      >
+        <svg
+          class="absolute bottom-0 overflow-hidden"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+          version="1.1"
+          viewBox="0 0 2560 100"
+          x="0"
+          y="0"
+        >
+        </svg>
+      </div>
+    </section>
+    <section class="relative bg-gray-900 py-8">
+      <div class="container mx-auto px-4">
+        <div
+          class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64"
+        >
+          <div class="px-6">
+            <div class="flex flex-wrap justify-center">
+              <div
+                class="w-full lg:w-3/12 px-4 py-6 lg:order-2 flex justify-center"
+              >
+                <div class="relative">
+                  <img
+                    alt="..."
+                    :src="JohnS"
+                    class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
+                    style="max-width: 150px;"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="text-center mt-12">
+              <h3
+                class="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2"
+              >
+                JohnS
+              </h3>
+              <div
+                class="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase"
+              >
+                <i
+                  class="fas fa-map-marker-alt mr-2 text-lg text-gray-500"
+                ></i>
+                Toronto, Ontairo
+              </div>
+              <div class="mb-2 text-gray-700">
+                <i class="fas fa-briefcase mr-2 text-lg text-gray-500"></i
+                >Developer for Brain Games
+              </div>
+            </div>
+            <div class="mt-10 py-10 border-t border-gray-300 text-center">
+              <div class="flex flex-wrap justify-center">
+                <div class="w-full lg:w-9/12 px-4">
+                  <p class="mb-4 text-lg leading-relaxed text-gray-800">
+                    Hi, I'm JohnS but you can call me Johnathon.
                   </p>
                 </div>
               </div>
@@ -167,7 +241,8 @@ export default {
     data () {
         return {
             perfectsquare150: require('@/assets/AboutUs/perfectsquare150.png'),
-            GrantBGreat: require('@/assets/AboutUs/GrantBGreat.jpg')
+            GrantBGreat: require('@/assets/AboutUs/GrantBGreat.jpg'),
+            JohnS: require('@/assets/AboutUs/JohnS.png')
         }
     },
 }
