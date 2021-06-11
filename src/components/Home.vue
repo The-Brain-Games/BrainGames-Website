850d023694ac8a32875e9d85283e5d0e916d297a
Grant <grant@rbgn.ca>
2021-06-10 23:35:53 -0400

    redo home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -36,6 +36,8 @@
         </div>
       </div>
     </div>
+
+
     <section class="text-gray-400 bg-gray-900 body-font">
         <div class="container flex flex-wrap px-5 py-24 mx-auto items-center">
           <div
@@ -67,50 +69,16 @@
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
             </router-link>
-          </div>
-          <div class="flex flex-col md:w-1/2 md:pl-12">
-            <center>
-              <iframe
-                class="w-full mb-20 max-w-md"
-                src="https://discord.com/widget?id=804162605107249213&theme=dark"
-                width="350"
-                height="500"
-                allowtransparency="true"
-                frameborder="0"
-                sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-              ></iframe>
-            </center>
-          </div>
-        </div>
-
-        <div class="container flex flex-wrap px-5 py-24 mx-auto items-center">
-          <div
-            class="md:w-1/2 md:pr-12 md:py-8 md:border-r md:border-b-0 md:mb-0 mb-10 pb-10 border-b border-gray-800"
-          >
+            <br><br>
             <h1
               class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white"
             >
               BrainNG
             </h1>
             <p class="leading-relaxed text-base">
-              The BrainNG Discord is a place for enthusiasts of the game Beam.NG
-              to play multiplayer on the BeamMP servers we host. We are proud to
-              host two of the most popular servers on the platform, one of which
-              including one of the largest modpacks on the platform! We host
-              events for people to take part in on our Discord and serve as a
-              place for these people to communicate.<br /><br />On top of the
-              BeamMP servers, this discord is also home to the
-              <a
-                href="https://top.gg/bot/784631695902375956"
-                style="color: #820d94"
-                target="_blank"
-                rel="noopener noreferrer"
-                >BeamMP Status discord bot</a
-              >. This bot is used by multiple other servers in the community for
-              pinging and getting information about BeamMP servers.<br /><br />On
-              the discord, we also welcome any general enthusiasts of car
-              related video games as we all tend to think alike and get along
-              pretty well.
+              The BrainNG server is a place for enthusiasts of the game BeamNG
+              to play. We are proud to host two great BeamMP servers, one of 
+              which including one of the largest modpacks on the platform!
             </p>
             <router-link to="/brainng" class="text-pink-400 inline-flex items-center mt-4"
               >Learn More
@@ -126,20 +94,49 @@
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
             </router-link>
+            <br><br>
+            <h1
+              class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white"
+            >
+              BeamMP Bot
+            </h1>
+            <p class="leading-relaxed text-base">
+              This discord is also home to the BeamMP Status discord bot. 
+              This bot is used by multiple other servers in the community for
+              pinging and getting information about BeamMP servers.
+            </p>
+            <a 
+              href="https://top.gg/bot/784631695902375956"
+              class="text-pink-400 inline-flex items-center mt-4"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Learn More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </a>
           </div>
-          <div class="flex flex-col md:w-1/2 md:pl-12">
-            <center>
-              <iframe
-                class="w-full mb-20 max-w-md"
-                src="https://discord.com/widget?id=789657841895735337&theme=dark"
-                width="350"
-                height="500"
-                allowtransparency="true"
-                frameborder="0"
-                sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-              ></iframe>
-            </center>
-          </div>
+            <div class="flex flex-col md:w-1/2 md:pl-12">
+              <center>
+                <iframe
+                  class="w-full mb-20 max-w-md"
+                  src="https://discord.com/widget?id=789657841895735337&theme=dark"
+                  width="350"
+                  height="600"
+                  allowtransparency="true"
+                  frameborder="0"
+                  sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+                ></iframe>
+              </center>
+            </div>
         </div>
       </section>
   </div>
